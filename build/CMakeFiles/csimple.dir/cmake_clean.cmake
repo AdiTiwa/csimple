8519447fdb7596bd58d4047c4f3e293d39f6ad04
shasahasr <shasahasr@gmail.com>
2023-05-07 17:51:27 -0500

    cpp lexer etc etc

---
+++ build/CMakeFiles/csimple.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csimple.dir/lexer.cpp.o"
+  "CMakeFiles/csimple.dir/lexer.cpp.o.d"
+  "CMakeFiles/csimple.dir/main.cpp.o"
+  "CMakeFiles/csimple.dir/main.cpp.o.d"
+  "csimple"
+  "csimple.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/csimple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
